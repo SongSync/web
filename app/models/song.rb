1794eb9ec08b228ebb36a0364e84099808a26ba2
Mavvie <tylerboydblue@gmail.com>
2014-09-20 02:11:20 -0400

    s3 and files

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :playlist
+  has_attached_file :file
 
   def serializable_hash(options={})
     super({
-      only: [:id, :name]
+      only: [:id, :name],
+      methods: [:file_url]
     }.merge(options))
+  end
+
+  def file_url
+    file.url
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: 'config/s3.yml'
+  }
 end
